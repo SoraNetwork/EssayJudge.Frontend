11476f1e1a2375a6c8c7db4df316c849e00be8ac
Yiri0731 <zhouxpking@qq.com>
2025-07-18 23:09:58 +0800

    本地跑了一下 npm install ，解决了（？）编译问题

--- dist/assets/VSpacer-BHD9tf2s.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{F as r}from"./VAvatar-B8y62MJr.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ dist/assets/VSpacer-Bt9Vw0ZL.js
@@ -0,0 +1,1 @@
+/* empty css              */import{O as r}from"./VAvatar-3Cr77brp.js";const p=r("v-spacer","div","VSpacer");export{p as V};
