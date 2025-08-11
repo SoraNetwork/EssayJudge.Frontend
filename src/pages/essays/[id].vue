a620e6aec0d046eca7fc9cdec1d563582fb46a79
Yiri0731 <zhouxpking@qq.com>
2025-08-11 22:38:36 +0800

    修复了（？）图片显示不出来的问题

--- src/pages/essays/[id].vue
+++ src/pages/essays/[id].vue
@@ -96,7 +96,7 @@
     <!-- Image Dialog -->
     <v-dialog v-model="imageDialog" max-width="800px">
       <v-card>
-        <v-img :src="`http://localhost:5000${essay?.imageUrl}`"></v-img>
+        <v-img :src="`${import.meta.env.VITE_API_BASE_URL}${essay?.imageUrl}`"></v-img>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="primary" text @click="imageDialog = false">关闭</v-btn>
